f4982d4a8645dd450fa221870ad0b089977274a1
Beverly Mugwadi <b.mugwadi1@alustudent.com>
2026-06-22 13:13:00 +0200

    add JWT token utility

---
+++ server/src/utils/token.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const jwt = require('jsonwebtoken');
+const env = require('../config/env');
+
+function signToken(userId) {
+  return jwt.sign({ id: userId }, env.jwtSecret, {
+    expiresIn: env.jwtExpiresIn,
+  });
+}
+
+function verifyToken(token) {
+  return jwt.verify(token, env.jwtSecret);
+}
+
+module.exports = { signToken, verifyToken };
